adfebeab4ab783186b2f310e0ef480189032c0d1
nikhilatexas <40574628+nikhilatexas@users.noreply.github.com>
2019-04-24 19:18:53 -0500

    add old files

---
+++ map.js
@@ -0,0 +1,34 @@
+
+var map;
+function initMap() {
+map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 30.2862723, lng: -97.7387818},
+    zoom: 8
+});
+}
+
+function updatePosition(lat, long) {
+    // actually update the marker
+    var myLatLng = {lat: lat, lng: long};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 20,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Backpack is Here!'
+        });
+}
+
+function getPosition() {
+    // TODO: connect to the arduino/ get the actual position
+    console.log("button")
+    lat = 30.2862723
+    long= -97.7387818
+    updatePosition(lat, long)
+}
+
+
